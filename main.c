aa23c8aa133ee9eb6680803c73a6d55b101a9994
milaronix <milaronix@gmail.com>
2014-11-03 17:54:01 -0600

    0 errores - 0 warnings
    se movio la declaracion de metodos para evitar los warnings

--- main.c
+++ main.c
@@ -21,97 +21,13 @@
 #define SYSAHBCLKCTRL (*((volatile unsigned long *) 0x40048080)) //System AHB clock control
 #define UARTCLKDIV (*((volatile unsigned long *) 0x40048098)) //UART clock divider
 
-void delay_us( int);
+/*void delay_us( int);
 void delay_ms( int);
 void desliza_texto(int posiciones);
 void splash_screen();
 void set_splash_screen();
 void imprime();
-void limpia_pantalla();
-
-int main(){
-	IOCON_PIO1_7 |= 0x01;			//TxD enable to output
-	//LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 12);
-	SYSAHBCLKCTRL |= (1 << 12); 	//Clock to UART
-	UARTCLKDIV |= 4; 				//48000000/4 = 12 MHz for UARTPCLK
-	U0FCR |= 0x01; 					//Enable FIFO buffer
-	U0LCR |= 1 << 7; 				//DLAB must be 1 to write to U0DLL, and U0FDR
-	//divAdd=7 mulVal=10 dll=23 //Set up baud rate
-	U0FDR = 0xA7; 					//DIVADDVAL is in bits 0-3 and MULVAL is in bits 4-7
-	U0DLL = 46;
-	U0LCR &= ~(1 << 7); 			//DLAB back to 0 to transmit
-	U0LCR |= 0x03; 					//8-bit data width
-	
-	delay_ms(2000);					//espera lo suficiente para que arranque el display LCD
-	imprime("    esto es un  mensaje estatico",100,0);	
-	delay_ms(1000);
-	limpia_pantalla();
-	delay_ms(1000);
-	imprime("este se puede deslizar",100,1);
-	delay_ms(1000);
-	limpia_pantalla();
-	delay_ms(1000);
-	imprime("y este este es infinito",100,2);
-	
-}
-
-void imprime(char *texto, int delay, int desliza){
-	int posicion = 0;
-	int largo = 0;
-	
-	if(desliza == 1){
-		while(texto[posicion++]){
-			largo++;
-		}
-	}
-	
-	posicion = 0;
-	while(texto[posicion]){		
-		if(posicion == 16){
-			if(desliza == 1 || desliza == 2){
-				U0THR = 0xfe;						//comando escape para poder correr el cursor
-				U0THR = 148;						//cambia a la primer posicion de la segunda linea
-				delay_ms(100);
-			}else{
-				U0THR = 0xfe;						//comando escape para poder correr el cursor
-				U0THR = 192;						//cambia a la primer posicion de la segunda linea
-			}
-		}
-		U0THR = texto[posicion];delay_ms(delay);  //imprime
-		posicion++;
-	}
-	
-	if(desliza == 1){
-		desliza_texto(largo);
-	}
-	if(desliza == 2){
-		desliza_texto(0xFFFFF);
-	}
-}
-
-void desliza_texto(int posiciones){
-	int i;
-	for(i=0; i <= posiciones; ++i){
-			U0THR = 0xfe;						//comando escape para poder correr el cursor
-			U0THR = 0X18;						//corrimiento cursor para centrar la frase
-			delay_ms(900);
-	}
-}
-
-void limpia_pantalla(){
-	U0THR = 0xfe;						//comando escape 
-	U0THR = 0x01;						//habilita splash screen
-}
-
-void splash_screen(){
-	U0THR = 0x7c;						//comando escape 
-	U0THR = 9;							//habilita splash screen
-}
-
-void set_splash_screen(){
-	U0THR = 0x7c;						//comando escape 
-	U0THR = 10;						//comando escape 
-}
+void limpia_pantalla();*/
 
 void delay_us(int delay)
 {
@@ -143,4 +59,88 @@ void delay_ms(int delay)
       delay_us(250);
     }
   }
+}
+
+void desliza_texto(int posiciones){
+	int i;
+	for(i=0; i <= posiciones; ++i){
+			U0THR = 0xfe;						//comando escape para poder correr el cursor
+			U0THR = 0X18;						//corrimiento cursor para centrar la frase
+			delay_ms(900);
+	}
+}
+
+void limpia_pantalla(){
+	U0THR = 0xfe;						//comando escape 
+	U0THR = 0x01;						//habilita splash screen
+}
+
+void splash_screen(){
+	U0THR = 0x7c;						//comando escape 
+	U0THR = 9;							//habilita splash screen
+}
+
+void set_splash_screen(){
+	U0THR = 0x7c;						//comando escape 
+	U0THR = 10;						//comando escape 
+}
+
+void imprime(char *texto, int delay, int desliza){
+	int posicion = 0;
+	int largo = 0;
+	
+	if(desliza == 1){
+		while(texto[posicion++]){
+			largo++;
+		}
+	}
+	
+	posicion = 0;
+	while(texto[posicion]){		
+		if(posicion == 16){
+			if(desliza == 1 || desliza == 2){
+				U0THR = 0xfe;						//comando escape para poder correr el cursor
+				U0THR = 148;						//cambia a la primer posicion de la segunda linea
+				delay_ms(100);
+			}else{
+				U0THR = 0xfe;						//comando escape para poder correr el cursor
+				U0THR = 192;						//cambia a la primer posicion de la segunda linea
+				delay_ms(50);
+			}
+		}
+		U0THR = texto[posicion];delay_ms(delay);  //imprime
+		posicion++;
+	}
+	
+	if(desliza == 1){
+		desliza_texto(largo);
+	}
+	if(desliza == 2){
+		desliza_texto(0xFFFFF);
+	}
+}
+
+int main(){
+	IOCON_PIO1_7 |= 0x01;			//TxD enable to output
+	//LPC_SYSCON->SYSAHBCLKCTRL |= (1 << 12);
+	SYSAHBCLKCTRL |= (1 << 12); 	//Clock to UART
+	UARTCLKDIV |= 4; 				//48000000/4 = 12 MHz for UARTPCLK
+	U0FCR |= 0x01; 					//Enable FIFO buffer
+	U0LCR |= 1 << 7; 				//DLAB must be 1 to write to U0DLL, and U0FDR
+	//divAdd=7 mulVal=10 dll=23 //Set up baud rate
+	U0FDR = 0xA7; 					//DIVADDVAL is in bits 0-3 and MULVAL is in bits 4-7
+	U0DLL = 46;
+	U0LCR &= ~(1 << 7); 			//DLAB back to 0 to transmit
+	U0LCR |= 0x03; 					//8-bit data width
+	
+	delay_ms(2000);					//espera lo suficiente para que arranque el display LCD
+	imprime("    esto es un  mensaje estatico",100,0);	
+	delay_ms(1000);
+	limpia_pantalla();
+	delay_ms(1000);
+	imprime("este se puede deslizar",100,1);
+	delay_ms(1000);
+	limpia_pantalla();
+	delay_ms(1000);
+	imprime("y este este es infinito",100,2);
 }
